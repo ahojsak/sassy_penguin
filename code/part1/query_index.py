71e01b6889059494de3fd39587606da3e5478981
Allison <allisonhojsak@gmail.com>
2014-04-27 16:44:55 -0400

    updated ranking

--- code/part1/query_index.py
+++ code/part1/query_index.py
@@ -24,13 +24,12 @@ def rank(businesses, words):
 	tfidf = {x: 0 for x in businesses}
 	for w in words:
 		for b in businesses:
-			reviews = index[w][b]
+			reviews = inverted_index[w][b]
 			tf = 0
-			#find correct entry in business dataset
-			for n, line in enumerate(f.readlines()):
-				data = json.loads(line.replace("\n",""))
-				if data["business_id"] == b:
-					total_reviews = data["review_count"]
+			
+			data = business_index[b]
+			total_reviews = data["review_count"]
+
 			#num of reviews in which word appears		
 			relevant_reviews = len(reviews)
 			for k, v in enumerate(reviews):
@@ -38,9 +37,7 @@ def rank(businesses, words):
 
 			idf = math.log(total_reviews / relevant_reviews)
 			tfidf[b] += tf * idf
-
-
-
+	
 	return scores
 
 
