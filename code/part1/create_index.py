8aa5f73dca257d3b6ff2228638f62feaa066a95c
Alison Gale <agale@cs.brown.edu>
2014-04-29 18:56:47 -0400

    Fixed up create index

--- code/part1/create_index.py
+++ code/part1/create_index.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import porter_stemmer
+import re,string
 
 # Script Usage: python create_index.py <review file> <stopword file> > <output file>
 
@@ -21,18 +22,26 @@ def main():
 	stemmed_dict = {}
 
 	# load stop word file and save
-	sf = open(review_file)
+	sf = open(stopword_file)
 	stopwords = {}
 	for line in sf.readlines():
 		stopwords[line.replace("\n","")] = 1
-	
+	sf.close()
+
+	regex = re.compile('[%s]' % re.escape(string.punctuation))
+
 	# process the review file
 	f = open(review_file)
-	for n, line in enumerate(f.readlines()):
-		data = json.loads(line.replace("\n",""))
-		business = data["business_id"]
+	n=0
+	line = f.readline()
+	while line:
+			
+		data = json.loads(line)
+		business = data['business_id']
+	
 		# lower case the word and split on white space
-		for i, word in enumerate(data["text"].lower().split()):
+		i=0
+		for word in regex.sub(' ', data["text"].lower()).split():	
 			# check to ensure word isn't a stop word
 			if word not in stopwords:
 				# apply porter stemming
@@ -50,7 +59,12 @@ def main():
 					index[word][business][n] = [i]
 				else:
 					index[word][business][n].append(i)
-	print json.dumps(index)
+			
+			i+=1
+		n+=1
+		line = f.readline()
+	f.close()
+	print json.dumps(index, separators=(',',':'))
 
 if __name__ == '__main__':
 	main()
